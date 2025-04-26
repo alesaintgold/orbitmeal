4aa2b92979325d3df77085f45364efd990cc3da3
alesaintgold <alexi.santoro@gmaio.com>
2025-04-26 16:22:13 +0200

    improved graphic and functionality

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 def load_collection_data(filename):
-    """Loads JSON data from a file in the 'collections' subfolder."""
     filepath = os.path.join(app.root_path, 'collections', filename)
     try:
         with open(filepath, 'r') as f:
@@ -18,14 +18,15 @@ def load_collection_data(filename):
         print(f"Error: Could not decode JSON from {filepath}")
         return None
 
+
 @app.route('/')
 def index():
-    """Loads data from 'my_collection.json' and renders it in a template."""
     collection_data = load_collection_data('recipes.json')
     if collection_data:
         return render_template('index.html', data=collection_data)
     else:
         return "Failed to load collection data."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
